f1625e1051b8f32f5d39869869c307315dc9ed94
shabil15 <mohammedshabil15@gmail.com>
2024-02-26 16:48:57 +0530

    store created and cakeSlice done

---
+++ index.js
@@ -0,0 +1,18 @@
+const store = require('./app/store')
+
+const cakeActions = require('./app/features/cake/cakeSlice').cakeActions
+
+
+console.log("initial State ",store.getState())
+
+const unsubscribe = store.subscribe(()=>{
+  console.log('updated state',store.getState());
+})
+
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+
+store.dispatch(cakeActions.restocked(3))
+
+unsubscribe()
