ca6ec9ca5f87e4c0ab741364b5eb052c574a4f7b
shabil15 <mohammedshabil15@gmail.com>
2024-02-26 18:42:19 +0530

    iceCream Slice ordered and restocked option done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const store = require('./app/store')
 
 const cakeActions = require('./app/features/cake/cakeSlice').cakeActions
-
+const icecreamActions = require('./app/features/icecream/icecreamSlice').icecreamActions
 
 console.log("initial State ",store.getState())
 
@@ -12,7 +12,10 @@ const unsubscribe = store.subscribe(()=>{
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
-
 store.dispatch(cakeActions.restocked(3))
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
 
+store.dispatch(icecreamActions.restocked(3))
 unsubscribe()
